521152b2df99e33abb8b6dabbd1dcbeb5e5662ef
Nathan Blum <n.blum@student.tudelft.nl>
2025-05-18 12:00:37 +0200

    Added release action

--- lib_ml/preprocessing.py
+++ lib_ml/preprocessing.py
@@ -13,9 +13,6 @@ from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.preprocessing import FunctionTransformer
-from sklearn.pipeline import make_union
 from joblib import dump
 
 try:
@@ -32,7 +29,7 @@ def _load_data(path: Path):
 
 def _text_process(text: str):
     all_stopwords = stopwords.words('english')
-    all_stopwords.remove('not')
+    all_stopwords.remove('not', 'but')
     ps = PorterStemmer()
 
     review = re.sub('[^a-zA-Z]', ' ', text)
@@ -43,20 +40,13 @@ def _text_process(text: str):
     
     return cln_review
 
-def _extract_message_len(data):
-    return np.array([len(message) for message in data]).reshape(-1, 1)
-
-
 def preprocess(path: Path):
     reviews = _load_data(path)
 
-    preprocessor = make_union(
-        CountVectorizer(max_features=1420,
-                        analyzer=_text_process),
-        FunctionTransformer(_extract_message_len, validate=False)
-    )
+    preprocessor = CountVectorizer(max_features=1420,
+                        analyzer=_text_process)
 
     preprocessed_data = preprocessor.fit_transform(reviews['Review'])
     dump(preprocessor, 'output/preprocessor.joblib')
     dump(preprocessed_data, 'output/preprocessed_data.joblib')
-    return preprocessed_data
+    return preprocessor, preprocessed_data
